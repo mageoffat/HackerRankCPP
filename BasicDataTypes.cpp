76842bc8d4aefe1051826fd0b51c65a3898afe51
Samuel Hastriter <61638381+mageoffat@users.noreply.github.com>
2020-06-02 16:57:44 -0600

    Create BasicDataTypes.cpp

---
+++ BasicDataTypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d", &a);
+    printf("%d\n", a);
+    scanf("%ld", &b);
+    printf("%ld\n", b);
+    cin >> c;
+    cout << c << endl;
+    scanf("%f", &d);
+    printf("%f\n", d);
+    scanf("%lf", &e);
+    printf("%lf", e);
+
+    return 0;
+}
